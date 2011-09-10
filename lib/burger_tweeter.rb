58b5bd37b0b72f57ddc2e5887e2ae3b97bd424d9
Leon Berenschot <leipeleon@gmail.com>
2011-09-10 17:27:15 +0200

    iterate over all items in each feed and check if we should tweet it. run the script once an hour

--- lib/burger_tweeter.rb
+++ lib/burger_tweeter.rb
@@ -29,8 +29,9 @@ end
 def upcoming_items
   rss = SimpleRSS.parse open(FEED[:upcoming_url])
   rss.items.each do |event|
-    opens_at = Time.parse(event.event_opens_at).utc
-    if (opens_at - Time.now.utc) < 3700 # kleiner dan 1 uur
+    opens_at = Time.parse(event.event_date).utc
+    should_we_tweet = (opens_at - Time.now.utc)
+    if should_we_tweet < 3600 && should_we_tweet > 0 # kleiner dan 1 uur
       snipped_link = snip(event.link)
       time = "#{opens_at.hour}:#{"%02d" % opens_at.min}"
       string = "Vandaag in @burgerweeshuis om #{time}: #{event.title}"[0...(120-snipped_link.size)]
@@ -43,26 +44,32 @@ end
 def expected_items
   rss = SimpleRSS.parse open(FEED[:expected_url])
   rss.items.each do |event|
-    snipped_link = snip(event.link)
-    date = Time.parse(event.event_date).utc
-    string = "Verwacht in @burgerweeshuis: #{event.title}, #{date.day} #{NL_ABBR_MONTHNAMES[date.month]} #{date.year}"[0...(120-snipped_link.size)]
-    string << " #{snipped_link}"
-    tweet_it(:expected, string)
+    should_we_tweet = (event.pubDate - Time.now.utc)
+    if should_we_tweet < 0 && should_we_tweet > -3600 # kleiner dan 1 uur
+      date = Time.parse(event.event_date).utc
+      snipped_link = snip(event.link)
+      string = "Verwacht in @burgerweeshuis: #{event.title}, #{date.day} #{NL_ABBR_MONTHNAMES[date.month]} #{date.year}"[0...(120-snipped_link.size)]
+      string << " #{snipped_link}"
+      tweet_it(:expected, string)
+    end
   end
 end
 
 def tweet_it(account_type, string)
   raise ArgumentError "Invalid Account Type (#{account_type})" unless [:upcoming, :expected].include?(account_type)
-  # puts "Passing to twitter: '#{string}'" 
-  # oauth = Twitter::OAuth.new(TWITTER[:key], TWITTER[:secret])
-  # oauth.authorize_from_access(TWITTER[account_type][:access_token], TWITTER[account_type][:access_secret])
-  # 
-  # tweeter = Twitter::Base.new(oauth)
-  # if tweeter.update(string)
-  #   event.update_attribute(:posted_at, DateTime.now) if event && account_type == :upcoming
-  #   sleep 120
-  # end
-  puts "Should pass to twitter: '#{string}'" 
+  if APP_ENV[:state] == 'production'
+    puts "Passing to twitter: '#{string}'"
+    oauth = Twitter::OAuth.new(TWITTER[:key], TWITTER[:secret])
+    oauth.authorize_from_access(TWITTER[account_type][:access_token], TWITTER[account_type][:access_secret])
+
+    tweeter = Twitter::Base.new(oauth)
+    if tweeter.update(string)
+      event.update_attribute(:posted_at, DateTime.now) if event && account_type == :upcoming
+      sleep 120
+    end
+  else
+    puts "Should pass to twitter: '#{string}'"
+  end
 end
 
 upcoming_items
